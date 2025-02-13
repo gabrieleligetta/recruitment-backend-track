f3bb9d2171f5fae51d1367290ca5d26be8024cca
gligetta <g.ligetta@notomia.com>
2025-02-13 23:13:08 +0100

    rudimentary jwt auth exceptions handle

--- src/app/Http/Controllers/AuthController.php
+++ src/app/Http/Controllers/AuthController.php
@@ -2,62 +2,57 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    // Endpoint per la registrazione (signup)
     public function signup(Request $request)
     {
-        // Validazione dei dati di input
         $validator = Validator::make($request->all(), [
-            'name'                  => 'required|string|max:255',
-            'email'                 => 'required|string|email|max:255|unique:users',
-            'password'              => 'required|string|min:6|confirmed',
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 422);
         }
 
-        // Creazione dell'utente
         $user = User::create([
-            'name'     => $request->name,
-            'email'    => $request->email,
+            'name' => $request->name,
+            'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        // Creazione del token JWT per l'utente appena creato
         $token = JWTAuth::fromUser($user);
 
         return response()->json([
-            'user'  => $user,
-            'token' => $token
+            'user' => $user,
+            'token' => $token,
         ], 201);
     }
 
-    // Endpoint per il login che rilascia il token JWT
     public function login(Request $request)
     {
         $credentials = $request->only('email', 'password');
 
-        if (!$token = JWTAuth::attempt($credentials)) {
+        if (! $token = JWTAuth::attempt($credentials)) {
             return response()->json(['error' => 'Credenziali non valide'], 401);
         }
 
         return response()->json([
-            'token' => $token
+            'token' => $token,
         ]);
     }
 
-    // Endpoint protetto che ritorna i dati dell'utente autenticato
     public function me()
+    : JsonResponse
     {
         return response()->json(auth()->user());
     }
 }
-
