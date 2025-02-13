fa1b2b0575ea7d7c91976546ca832002217b5b59
gligetta <g.ligetta@notomia.com>
2025-02-13 22:08:06 +0100

    merged base and build stage in dockerfile, also moved project to /var/www/invoices_app

---
+++ src/database/migrations/2025_02_13_165322_create_tax_profiles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tax_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('tax_id')->unique();
+            $table->string('company_name');
+            $table->string('address');
+            $table->string('country');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tax_profiles');
+    }
+};
